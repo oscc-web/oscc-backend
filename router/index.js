acc02db0c0cf97e3647db06f424e54dcb1d6ae3b
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2022-02-21 07:28:25 +0000

    Infrastructure update

--- router/index.js
+++ router/index.js
@@ -1,25 +1,33 @@
-// Import code modules
-import { init, resolveDistPath, config, logger } from '../lib/env.js'
-import '../lib/extendHttp.js'
-import vhost from './middleware/vhost.js'
-// Import package modules
+// Imports
+import { init, resolveDistPath, config, logger, internalCookieRx } from '../lib/env.js'
 import express from 'express'
+import vhost from './middleware/vhost.js'
 import proxy from './middleware/proxy.js'
+import Session from '../lib/session.js'
 // Extract related configs from user config
-const port = config?.port?.router || 80
+const port = config?.port?.router || 8080
 // Environment setup
 init(import.meta)
 // Boot-up log
 logger.info(`YSYX backend services launched at [${config.mode}] mode`)
 // Compose the server
 express()
+	// Remove express powered-by header
+	.disable('x-powered-by')
 	// Preprocessor and access logger
 	.use((req, res, next) => {
 		logger.access(`${req.method} ${req.headers.host}${req.url} from ${req.origin}`)
 		// Filter request cookies specified by stripeCookiePrefix
 		// these cookies are likely to be used internally,
 		// and should never be passed through to application servers
-		req.filterCookies(name => !config.stripeCookiePrefix.test(name))
+		let filteredCookies = req.filterCookies(
+			name => !internalCookieRx.test(name)
+		)
+		if (filteredCookies.length) logger.errAcc(
+			'Internal cookies found in request, will be removed',
+			filteredCookies
+		)
+		// Pass down the request
 		next()
 	})
 	// YSYX.ORG
@@ -40,30 +48,46 @@ express()
 	// FORUM
 	.use(vhost(
 		/^forum.ysyx.(org|cc|dev|local)$/i,
-		proxy(req => {
-			// // Verify if visitor has an active session 
-			// let sessionToken = Session.verify(req)
-			// // Inject user info into the request if session is active
-			// if (sessionToken) Session.injectSessionToken(req, sessionToken)
-			// Rewrite target
-			return {
+		express().disable('x-powered-by').use(
+			// Session preprocessor
+			Session.preprocessor,
+			// Forward processed traffic to real NodeBB service
+			proxy(req => ({
 				hostname: '127.0.0.1',
 				port: config?.port?.NodeBB || 4567,
 				path: req.url,
 				method: req.method,
 				headers: req.headers
-			}
-		})
+			}))
+		)
 	))
-	// Deploy server
+	// API server
 	.use(vhost(
-		/^deploy.ysyx.(org|cc|dev|local)$/i,
-		express.static(resolveDistPath('ysyx'))
+		/^api.ysyx.(org|cc|dev|local)$/i,
+		express().use(
+			// Session preprocessor
+			Session.preprocessor,
+			// Forward processed traffic to real NodeBB service
+			proxy(req => ({
+				hostname: '127.0.0.1',
+				port: config?.port?.api || 8999,
+				path: req.url,
+				method: req.method,
+				headers: req.headers
+			}))
+		)
 	))
 	// Uncaught Request handler
 	.use((req, res) => {
-		res.sendStatus(404)
+		res.status(404)
 		logger.errAcc(`Unable to handle request ${req.headers.host}${req.url} from ${req.origin}`)
+		// respond with html page
+		if (req.accepts('html')) {
+			res.render('404', { url: req.url })
+			return
+		}
+		// fallback to plain-text
+		else res.type('txt').send('404 Not found')
 	})
 	// Open listening port
 	.listen(port, () => logger.info(`service up and running at port ${port}`))
