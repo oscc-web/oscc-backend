2f605f84e9e90b0cfc1f708e65ab6a1933d25dbf
wxl <994119862@qq.com>
2022-02-23 08:19:15 +0000

    modules/mailer: update mailer

--- tests/mailer.js
+++ tests/mailer.js
@@ -4,8 +4,6 @@ import 'colors'
 
 init(import.meta)
 const TIME_OUT = 2000
-await new Promise(res => setTimeout(() => res(), TIME_OUT))
-console.log('starting test server'.green)
 let payload = {
 	template: 'validateEmail',
 	to: 'wxl994119862@stu.zzu.edu.cn',
@@ -21,7 +19,6 @@ let requestOptions = {
 	body: JSON.stringify(payload),
 	mode: 'cors'
 }
-console.log(JSON.stringify(requestOptions).green)
 console.log('Test: fetch mailer response code is 200, returns as expected'.green)
 fetch(
 	'http://127.0.0.1:9998',
@@ -35,12 +32,26 @@ await new Promise(res => setTimeout(() => res(), TIME_OUT))
 payload.template = 'noExistTemplate'
 requestOptions.body = JSON.stringify(payload)
 console.log('Test: fetch mailer response code is 500, returns as expected'.green)
-// fetch(
-// 	'http://127.0.0.1:9998',
-// 	requestOptions
-// ).then(res => {
-// 	console.log(JSON.stringify(res.status).blue)
-// }).catch(e => {
-// 	console.log(e.message.red)
-// })
+fetch(
+	'http://127.0.0.1:9998',
+	requestOptions
+).then(res => {
+	console.log(JSON.stringify(res.status).blue)
+}).catch(e => {
+	console.log(e.message.red)
+})
+await new Promise(res => setTimeout(() => res(), TIME_OUT))
+requestOptions.method = "GET"
+requestOptions.body = null
+console.log('Test: fetch mailer response code is 404, returns as expected'.green)
+fetch(
+	'http://127.0.0.1:9998',
+	requestOptions
+).then(res => {
+	console.log(JSON.stringify(res.status).blue)
+}).catch(e => {
+	console.log(e.message.red)
+})
+await new Promise(res => setTimeout(() => res(), TIME_OUT))
+console.log('Test end'.green)
 
