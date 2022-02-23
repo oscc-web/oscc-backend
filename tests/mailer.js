c75433458a111692e57a75090097508f42da2a0e
wxl <994119862@qq.com>
2022-02-23 13:39:45 +0000

    module/malier remove node-fectch and use http.request

--- tests/mailer.js
+++ tests/mailer.js
@@ -1,9 +1,17 @@
 import { init } from '../lib/env.js'
-import fetch from 'node-fetch'
+import http from 'http'
 import 'colors'
 
 init(import.meta)
 const TIME_OUT = 2000
+let options = {
+	hostname: '127.0.0.1',
+	port: 9998,
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+	}
+}
 let payload = {
 	template: 'validateEmail',
 	to: 'wxl994119862@stu.zzu.edu.cn',
@@ -11,47 +19,27 @@ let payload = {
 		link: 'ysyx.org/register?token=token123456'
 	}
 }
-let requestOptions = {
-	method: 'POST',
-	headers: {
-		'Content-Type': 'application/json',
-	},
-	body: JSON.stringify(payload),
-	mode: 'cors'
-}
 console.log('Test: fetch mailer response code is 200, returns as expected'.green)
-fetch(
-	'http://127.0.0.1:9998',
-	requestOptions
-).then(res => {
-	console.log(JSON.stringify(res.status).blue)
-}).catch(e => {
-	console.log(e.message.red)
+let req = http.request(options, res => {
+	console.log('Status: ' + res.statusCode);
 })
+req.write(JSON.stringify(payload))
+req.end()
 await new Promise(res => setTimeout(() => res(), TIME_OUT))
 payload.template = 'noExistTemplate'
-requestOptions.body = JSON.stringify(payload)
 console.log('Test: fetch mailer response code is 500, returns as expected'.green)
-fetch(
-	'http://127.0.0.1:9998',
-	requestOptions
-).then(res => {
-	console.log(JSON.stringify(res.status).blue)
-}).catch(e => {
-	console.log(e.message.red)
+req = http.request(options, res => {
+	console.log('Status: ' + res.statusCode);
 })
+req.write(JSON.stringify(payload))
+req.end()
 await new Promise(res => setTimeout(() => res(), TIME_OUT))
-requestOptions.method = 'GET'
-requestOptions.body = null
+options.method = 'GET'
 console.log('Test: fetch mailer response code is 404, returns as expected'.green)
-fetch(
-	'http://127.0.0.1:9998',
-	requestOptions
-).then(res => {
-	console.log(JSON.stringify(res.status).blue)
-}).catch(e => {
-	console.log(e.message.red)
+req = http.request(options, res => {
+	console.log('Status: ' + res.statusCode);
 })
+req.end()
 await new Promise(res => setTimeout(() => res(), TIME_OUT))
 console.log('Test end'.green)
 
