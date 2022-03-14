f4c0ed275fdc6da79e85d40c7d9cd667d1916659
Wang XinLong <994119862@qq.com>
2022-03-14 01:09:56 +0000

    module/api: add user/avatar

--- modules/upload/index.js
+++ modules/upload/index.js
@@ -10,8 +10,6 @@ init(import.meta)
 const tempPath = `${PROJECT_ROOT}/tmp`
 // const storagePath = `${PROJECT_ROOT}/storage`
 // database init
-import dbInit from '../../utils/mongo.js'
-let db = await dbInit('upload/crud')
 logger.info('Staring upload server')
 let app = express()
 app.use((req, res, next) => {
@@ -37,54 +35,9 @@ app.use('/', (req, res) => {
 				logger.warn('save file error:', err.stack)
 				res.status(500).send('save file error')
 			} else {
-				try {
-					let records = await db.upload.find({ userID: user.id, for: filePurpose }).toArray()
-					if (records.length) {
-						records.forEach(file => {
-							fs.removeSync(tempPath + '/' + file._id)
-						})
-						await db.upload.delete({ userID: user.id, for: filePurpose })
-					}
-					await db.upload.insert({ _id: files.fileUpLoad.newFilename, userID: user.id, createTime: new Date().getTime(), for: filePurpose, type: 'temp' })
-				} catch (e) {
-					return logger.warn(`Failed to insert file information into database: ${e.stack}`) && res.status(500).send()
-				}
-				res.send(files.fileUpLoad.newFilename)
+				res.send(files.fileUpload.newFilename)
 			}
 		})
-	} else if (req.method === 'PUT') {
-		const body = []
-		logger.access(`${req.method} ${req.headers.host}${req.url} from ${req.origin}`)
-		req
-			.on('data', chunk => body.push(chunk))
-			.on('end', () => {
-				const payload = JSON.parse(body.join(''))
-				if (!payload || typeof payload !== 'object')
-					// Payload is not a valid JSON object
-					return logger.warn('Request payload is not an JSON object: ' + JSON.stringify(payload)) && res.status(400).send()
-				if (payload.fileType !== 'persistent' && payload.fileType !== 'temp')
-					return logger.warn('fileType is invalid: ' + JSON.stringify(payload)) && res.status(400).send()
-				// update record of file in database
-				db.upload
-					.find({ _id: payload.fileID })
-					.toArray()
-					.then(result => {
-						if (!result.length) {
-							logger.warn(`File not found _id: ${payload.fileID}`)
-							res.status(404).send()
-						} else {
-							db.upload
-								.update({ _id: payload.fileID }, { $set: { type: payload.fileType } })
-								.then(() => {
-									res.status(200).send()
-								})
-								.catch(e => {
-									logger.warn(`Failed to update file _id: ${payload.fileID}, error: ${e.stack}`)
-									res.status(500).send()
-								})
-						}
-					})
-			})
 	} else {
 		logger.warn(`Rejected ${req.method} request from ${req.origin}`)
 		res.status(405).send()
@@ -94,18 +47,18 @@ app.listen(config.port.upload, () => {
 	logger.info(`Upload server up and running at port ${config.port.upload}`)
 })
 // scheduled deletion
-const expireTime = config.upload.expireTime //milliseconds
-const job = new CronJob(config.upload.cron, () => {
-	let now = new Date().getTime()
-	db.upload
-		.find({ type: 'temp', createTime: { $lt: now - expireTime } })
-		.toArray()
-		.then(files => {
-			files.forEach(file => fs.remove(`${PROJECT_ROOT}/tmp/${file._id}`))
-			db.upload.delete({ type: 'temp', createTime: { $lt: now - expireTime } })
-		})
-		.catch(e => {
-			logger.warn(`scheduled file deletion error: ${e.stack}`)
-		})
-}, null, true, 'Asia/Shanghai')
-job.start()
+// const expireTime = config.upload.expireTime //milliseconds
+// const job = new CronJob(config.upload.cron, () => {
+// 	let now = new Date().getTime()
+// 	db.upload
+// 		.find({ type: 'temp', createTime: { $lt: now - expireTime } })
+// 		.toArray()
+// 		.then(files => {
+// 			files.forEach(file => fs.remove(`${PROJECT_ROOT}/tmp/${file._id}`))
+// 			db.upload.delete({ type: 'temp', createTime: { $lt: now - expireTime } })
+// 		})
+// 		.catch(e => {
+// 			logger.warn(`scheduled file deletion error: ${e.stack}`)
+// 		})
+// }, null, true, 'Asia/Shanghai')
+// job.start()
