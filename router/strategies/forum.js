7799096ae59409d004e9c47d810731a7d82dba0d
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2022-03-28 17:22:04 +0800

    router/strategies: update forum token format

--- router/strategies/forum.js
+++ router/strategies/forum.js
@@ -5,6 +5,7 @@ import { PRIV } from '../../lib/privileges.js'
 import Session from '../../lib/session.js'
 import withSession from '../../lib/middleware/withSession.js'
 import proxy from '../../lib/middleware/proxy.js'
+import logger from '../../lib/logger.js'
 
 export const forumGroupPrivLUT = Object.freeze([
 	[PRIV.FORUM_ADMIN, 'administrators'],
@@ -16,25 +17,18 @@ export const forumGroupPrivLUT = Object.freeze([
 export async function forumPreprocessor(req, res, next) {
 	const { session } = req
 	if (session instanceof Session) {
-		const header = {
-				'alg': 'HS256',
-				'typ': 'JWT'
-			},
-			user = await session.user,
+		const user = await session.user,
 			{ name, mail, userID } = user,
 			userData = { userID, name, mail },
-			groups = []
-		forumGroupPrivLUT.forEach(([priv, groupName]) => {
-			if (user.hasPriv(priv)) groups.push(groupName)
-		})
-		const secret = config?.nodebb?.secret || 'forum.ysyx.secret'
-		req.injectCookies({
-			__internal_nodebb__: jsonwebtoken.sign(
-				{ ...userData, groups },
-				secret,
-				{ header }
-			)
-		})
+			groups = (await Promise.all(forumGroupPrivLUT.map(
+				async ([priv, groupName]) => {
+					if (await user.hasPriv(priv)) return groupName
+					return undefined
+				}
+			))).filter(group => !!group),
+			content = { ...userData, groups }
+		req.injectInternalCookies({ nodebb: JSON.stringify(content) })
+		logger.debug(`Forum preprocessor injected ${JSON.stringify(content)}`)
 	}
 	return next()
 }
