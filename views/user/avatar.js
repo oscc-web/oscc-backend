e0671b2f19a5b9b078e9a723366d4dd82cd3e6c6
Wang XinLong <994119862@qq.com>
2022-03-30 07:49:36 +0000

    get avatar

---
+++ views/user/avatar.js
@@ -0,0 +1,20 @@
+import { AppDataWithFs } from 'lib/appDataWithFs.js'
+import { statusCode } from 'lib/status.code.js'
+import logger from 'lib/logger.js'
+let appDataWithFs = new AppDataWithFs('user-profile')
+export async function getUserAvatar(req, res, next) {
+	const { session } = req, user = await session.user,
+		ownerID = req?.userID
+	appDataWithFs
+		.loadFile({ userID: ownerID, url: '/avatar' })
+		.then(async fileDescriptor => {
+			if (!fileDescriptor) return res.sendStatus(statusCode.ClientError.NotFound)
+			// pipe file to res
+			fileDescriptor.pipe(res)
+			logger.access(`send file ${fileDescriptor.fileID} to <${req.origin}>`)
+		})
+		.catch(e => {
+			logger.errAcc(`Can not get ${ownerID}'s avatar: ${e.stack}`)
+			res.sendStatus(statusCode.ServerError.InternalServerError)
+		})
+}
